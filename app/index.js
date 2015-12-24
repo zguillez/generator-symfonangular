48df5bc70263e9e3058f0ca6f48f2a231073508c
Guillermo de la Iglesia <zguillez@gmail.com>
2015-12-24 17:12:12 +0100

    update dependences

--- app/index.js
+++ app/index.js
@@ -2,27 +2,21 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
-
 module.exports = yeoman.generators.Base.extend({
-  initializing: function () {
+  initializing: function() {
     this.pkg = require('../package.json');
   },
-
-  prompting: function () {
+  prompting: function() {
     var done = this.async();
-
     this.log(yosay('Welcome to the fabulous ' + chalk.red('Symfonangular') + ' generator!'));
-
     var prompts = [];
-
-    this.prompt(prompts, function (props) {
+    this.prompt(prompts, function(props) {
       this.props = props;
       done();
     }.bind(this));
   },
-
   writing: {
-    app: function () {
+    app: function() {
       this.fs.copy(this.templatePath('composer.*'), this.destinationPath(''));
       this.fs.copy(this.templatePath('package.json'), this.destinationPath('package.json'));
       this.fs.copy(this.templatePath('bower.json'), this.destinationPath('bower.json'));
@@ -32,18 +26,15 @@ module.exports = yeoman.generators.Base.extend({
       this.fs.copy(this.templatePath('web'), this.destinationPath('web'));
       this.fs.copy(this.templatePath('grunt'), this.destinationPath('grunt'));
     },
-
-    projectfiles: function () {
+    projectfiles: function() {
       this.fs.copy(this.templatePath('editorconfig'), this.destinationPath('.editorconfig'));
       this.fs.copy(this.templatePath('jshintrc'), this.destinationPath('.jshintrc'));
       this.fs.copy(this.templatePath('bowerrc'), this.destinationPath('.bowerrc'));
-
     }
   },
-
-  install: function () {
+  install: function() {
     var scope = this;
-    this.installDependencies(function () {
+    this.installDependencies(function() {
       scope.spawnCommand('./composer.phar', ['self-update']);
       scope.spawnCommand('./composer.phar', ['install']);
     });
