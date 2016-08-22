0f02871961be08ed4c93be90285efe78d90215f9
Guillermo de la Iglesia <zguillez@gmail.com>
2016-08-22 13:54:16 +0200

    fix generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,20 +3,15 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 module.exports = yeoman.Base.extend({
-	initializing: function() {
-		this.pkg = require('../../package.json');
+	constructor: function () {
+		yeoman.Base.apply(this, arguments);
 	},
-	prompting: function() {
-		var done = this.async();
+	initializing: function () {
+		this.pkg = require('../../package.json');
 		this.log(yosay('Welcome to the zetadelic ' + chalk.red('Symfonangular v' + this.pkg.version) + ' generator!'));
-		var prompts = [];
-		this.prompt(prompts, function(props) {
-			this.props = props;
-			done();
-		}.bind(this));
 	},
 	writing: {
-		app: function() {
+		app: function () {
 			this.fs.copy(this.templatePath('composer.*'), this.destinationPath(''));
 			this.fs.copy(this.templatePath('package.json'), this.destinationPath('package.json'));
 			this.fs.copy(this.templatePath('bower.json'), this.destinationPath('bower.json'));
@@ -29,18 +24,16 @@ module.exports = yeoman.Base.extend({
 			this.fs.copy(this.templatePath('var'), this.destinationPath('var'));
 			this.fs.copy(this.templatePath('web'), this.destinationPath('web'));
 		},
-		projectfiles: function() {
+		projectfiles: function () {
 			this.fs.copy(this.templatePath('editorconfig'), this.destinationPath('.editorconfig'));
 			this.fs.copy(this.templatePath('jshintrc'), this.destinationPath('.jshintrc'));
 			this.fs.copy(this.templatePath('bowerrc'), this.destinationPath('.bowerrc'));
 		}
 	},
-	install: function() {
+	install: function () {
 		var scope = this;
-		this.installDependencies(function() {
-			console.log('\n\n' + chalk.underline.green('** Install Done! **') + '\n\n' + chalk.green(
-				'Install Symfony vendors running this command:') + '\n' + chalk.yellow(
-				'./composer.phar self-update && ./composer.phar install') + '\n\n');
+		this.installDependencies(function () {
+			console.log('\n\n' + chalk.underline.green('** Install Done! **') + '\n\n' + chalk.green('Install Symfony vendors running this command:') + '\n' + chalk.yellow('./composer.phar self-update && ./composer.phar install') + '\n\n');
 		});
 	}
 });
