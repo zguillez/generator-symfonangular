1cd6db6e7d63a50ec2057f11efc95ba812203b3d
Guillermo de la Iglesia <zguillez@gmail.com>
2016-08-22 12:53:44 +0200

    fixes

--- test/app.js
+++ test/app.js
@@ -2,11 +2,3 @@
 var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
-
-describe('generator-generator:app', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
-      .on('end', done);
-  });
-});
